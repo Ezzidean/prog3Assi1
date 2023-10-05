7ab28cd857c7b62c84b34c4c76c1391542e6ee3d
Ezzidean <ezzidean1@gmail.com>
2023-10-05 17:34:18 -0600

    Combined code with jareds version with mine

--- A1.java
+++ A1.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -25,6 +25,7 @@ public class A1 {
 	private Scanner input = new Scanner(System.in);
 	private int totalwordcount = 0;
 	private ArrayList<Avenger> avengersArrayList = new ArrayList<>();
+	private Map<String, Avenger> avengerMap = new HashMap<>();
 
 	public static void main(String[] args) {
 		A1 a1 = new A1();
@@ -33,105 +34,153 @@ public class A1 {
 
 	public void run() {
 		readInput();
-		printResults();
 	}
 
 	/**
-	 * read the input stream and keep track how many times avengers are mentioned by
-	 * alias or last name or performer's last name.
+	 * Read the input stream and keep track of how many times avengers are mentioned
+	 * by alias or last name or performer's last name.
 	 */
 	private void readInput() {
-		/*
-		 * In a loop, while the scanner object has not reached end of stream,
-		 * 
-		 * - read a word. - clean up the word - if the word is not empty, add the word
-		 * count. - Check if the word is either an avenger alias or last name, or
-		 * performer last name then - Create a new avenger object with the corresponding
-		 * alias and last name and performer last name. - if this avenger has already
-		 * been mentioned, increase the corresponding frequency count for the object
-		 * already in the list. - if this avenger has not been mentioned before, add the
-		 * newly created avenger to the list, remember to update the corresponding
-		 * frequency.
-		 */
-		int wordCount = 0;
-		int words = 0;
-		for (int i = 0; i <= words; i++) {
-			File input = new File("input1.txt");
-			try {
-				Scanner fileReader = new Scanner(input);
-				while (fileReader.hasNext()) {
-					ArrayList<String> Avengers = new ArrayList<String>();
+		while (input.hasNext()) {
+			String word = input.next();
+			// Step 1: Remove leading and trailing spaces, convert to lowercase
+			word = word.trim().toLowerCase();
 
-					String newWord = fileReader.next().toLowerCase().trim();
-					if (!newWord.matches("[^a-z]*")) {
-						if (!newWord.matches("^'[a-z]*")) {
-							Avengers.add(newWord);
-							wordCount += 1;
-
-							// int j = newWord.indexOf("'");
-							String[] arr = newWord.split("'");
-							newWord = arr[0];
-							// newWord = newWord.substring(0, j);
-
-						} else {
-							continue;
-						}
-					}
-
-					System.out.println(Avengers);
-
-				}
-				System.out.println(wordCount);
-			} catch (FileNotFoundException e) {
-
-				System.out.println("Wrong file address");
-				run();
+			// Step 2: Handle apostrophes
+			if (word.contains("'")) {
+				word = word.split("'")[0];
 			}
 
+			// Step 3: Remove punctuation and digits
+			word = word.replaceAll("[^a-z]", "");
+
+			// Step 4: Check if the word is not empty
+			if (!word.isEmpty()) {
+				totalwordcount++;
+
+				// Step 5: Check if it's an avenger
+				for (String[] avengerInfo : avengerRoster) {
+					for (String avengerName : avengerInfo) {
+						if (word.equals(avengerName)) {
+							updateAvengerCounts(avengerInfo);
+							break;
+						}
+					}
+				}
+			}
 		}
 	}
 
-	/*
-	 * Create helper functions as needed
-	 */
-
 	/**
-	 * print the results
+	 * Update the counts for the avenger found in the input.
+	 *
+	 * @param avengerInfo The information about the avenger.
 	 */
-	private void printResults() {
-		/*
-		 * Make sure your format matches the sample output exactly. No extra empty
-		 * lines, or text. Use the diff command to check if your output matches the
-		 * sample outputs.
+	private void updateAvengerCounts(String[] avengerInfo) {
+		String alias = avengerInfo[0];
+		String lastName = avengerInfo[1];
+		String performerLastName = avengerInfo[2];
+
+		Avenger avenger = avengerMap.get(alias);
+
+		if (avenger == null) {
+			avenger = new Avenger(alias, lastName, performerLastName);
+			avengersArrayList.add(avenger);
+			avengerMap.put(alias, avenger);
+		}
+
+		avenger.incrementAliasCount();
+		avenger.incrementLastNameCount();
+		avenger.incrementPerformerCount();
+	}
+
+	class Avenger {
+		private String alias;
+		private String lastName;
+		private String performerLastName;
+		private int aliasCount;
+		private int lastNameCount;
+		private int performerCount;
+
+		public Avenger(String alias, String lastName, String performerLastName) {
+			this.alias = alias;
+			this.lastName = lastName;
+			this.performerLastName = performerLastName;
+			this.aliasCount = 0;
+			this.lastNameCount = 0;
+			this.performerCount = 0;
+		}
+
+		public String getAlias() {
+			return alias;
+		}
+
+		public int getAliasCount() {
+			return aliasCount;
+		}
+
+		public int getLastNameCount() {
+			return lastNameCount;
+		}
+
+		public int getPerformerCount() {
+			return performerCount;
+		}
+
+		public int getTotalCount() {
+			return aliasCount + lastNameCount + performerCount;
+		}
+
+		public void incrementAliasCount() {
+			aliasCount++;
+		}
+
+		public void incrementLastNameCount() {
+			lastNameCount++;
+		}
+
+		public void incrementPerformerCount() {
+			performerCount++;
+		}
+
+		/**
+		 * print the results
 		 */
-		System.out.println("Total number of words: " + totalwordcount);
-		System.out.println("Number of Avengers Mentioned: " + avengersArrayList.size());
-		System.out.println();
+		private void printResults() {
+			/*
+			 * Make sure your format matches the sample output exactly. No extra empty
+			 * lines, or text. Use the diff command to check if your output matches the
+			 * sample outputs.
+			 */
+			System.out.println("Total number of words: " + totalwordcount);
+			System.out.println("Number of Avengers Mentioned: " + avengersArrayList.size());
+			System.out.println();
 
-		// Print all ordered by appearance
-		System.out.println("All avengers in the order they appeared in the input stream:");
-		// Todo: Print the list of avengers in the order they appeared in the input
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
+			// Print all ordered by appearance
+			System.out.println("All avengers in the order they appeared in the input stream:");
+			// Todo: Print the list of avengers in the order they appeared in the input
+			// Make sure you follow the formatting example in the sample output
+			System.out.println();
 
-		// Print all ordered by AvengerComparatorFreqDesc
-		System.out.println("Top " + topN + " most popular avengers:");
-		// Todo: Print the topN most popular avengers, see the instructions for tie
-		// breaking
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
+			// Print all ordered by AvengerComparatorFreqDesc
+			System.out.println("Top " + topN + " most popular avengers:");
+			// Todo: Print the topN most popular avengers, see the instructions for tie
+			// breaking
+			// Make sure you follow the formatting example in the sample output
+			System.out.println();
 
-		// Print top n ordered by AvengerPerformerComparatorFreqDesc
-		System.out.println("Top " + topN + " most popular performers:");
-		// Todo: Print the topN most popular performers, see the instructions for tie
-		// breaking
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
+			// Print top n ordered by AvengerPerformerComparatorFreqDesc
+			System.out.println("Top " + topN + " most popular performers:");
+			// Todo: Print the topN most popular performers, see the instructions for tie
+			// breaking
+			// Make sure you follow the formatting example in the sample output
+			System.out.println();
 
-		// Print all ordered by alias alphabetically
-		System.out.println("All mentioned avengers in alphabetical order:");
-		// Todo: Print the list of avengers in alphabetical order
-		// Make sure you follow the formatting example in the sample output
-		System.out.println();
+			// Print all ordered by alias alphabetically
+			System.out.println("All mentioned avengers in alphabetical order:");
+			// Todo: Print the list of avengers in alphabetical order
+			// Make sure you follow the formatting example in the sample output
+			System.out.println();
+		}
 	}
 }
